f2c576af6b0a09b8ac90a971ad854811275bf497
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2026-02-28 17:03:26 +0100

    feat: refactor SkillCard and ToolCard components to use Card.Header and ScrollShadow for improved structure and visual consistency

--- src/app/components/content.components/ListCard.skills.tsx
+++ src/app/components/content.components/ListCard.skills.tsx
@@ -1,6 +1,6 @@
 import { ComponentBaseProps, TagItem } from "@/lib/types";
 import { cn } from "@/lib/utils";
-import { Card, Tag, TagGroup } from "@heroui/react";
+import { Card, ScrollShadow, Tag, TagGroup } from "@heroui/react";
 import hardSkills from "@data/skills.hard.json";
 import softSkills from "@data/skills.soft.json";
 import { Tabs } from "@heroui/react";
@@ -13,10 +13,6 @@ export default function SkillCard({ className }: ComponentBaseProps) {
         "glass",
         "glass-white",
         "w-full",
-
-        // "max-w-fit",
-        // "max-h-100",
-        "",
         "",
         "",
 
@@ -24,84 +20,91 @@ export default function SkillCard({ className }: ComponentBaseProps) {
       )}
     >
       <Tabs className="w-full  gap-0 ">
-        <h2 className="text-secondary/85 self-center">Skills </h2>
-        <Tabs.ListContainer className="flex gap-2 h-fit bg-transparent">
-          <Tabs.List
-            aria-label="Options"
-            className={cn(
-              "h-fit",
-              // "glass",
-              "bg-transparent",
-              "mt-0.5",
-              "text-sm",
-              "text-secondary/85",
-
-              "",
-            )}
-          >
-            <Tabs.Tab
-              id="hard-skills"
-              className="h-fit text-secondary/85 font-normal"
-            >
-              Hard
-              <Tabs.Indicator
-              // className="glass bg-glass-light-gray"
-              />
-            </Tabs.Tab>
-            <Tabs.Tab
-              id="soft-skills"
-              className="h-fit text-secondary/85 font-normal"
-            >
-              Soft
-              <Tabs.Indicator
-              // className="glass bg-glass-light-gray"
-              />
-            </Tabs.Tab>
-          </Tabs.List>
-        </Tabs.ListContainer>
-        <Tabs.Panel id="hard-skills">
-          <TagGroup
-            variant="surface"
-            aria-label="Tags"
-            selectionMode="single"
-            className={cn("overflow-auto max-h-48", "", "")}
-          >
-            <TagGroup.List>
-              {hardSkills.map((skill: TagItem) => (
-                <Tag
-                  key={skill.id}
-                  id={skill.id}
-                  textValue={skill.name}
-                  className="font-light! px-2 py-0"
+        <Card.Header>
+          <Card.Title className="text-secondary/85 self-center">
+            Skills
+          </Card.Title>
+          <Card.Description>
+            <Tabs.ListContainer className="flex gap-2 h-fit bg-transparent">
+              <Tabs.List
+                aria-label="Options"
+                className={cn(
+                  "h-fit",
+                  "bg-transparent",
+                  "mt-0.5",
+                  "text-sm",
+                  "text-secondary/85",
+                  "",
+                )}
+              >
+                <Tabs.Tab
+                  id="hard-skills"
+                  className="h-fit text-secondary/85 font-normal"
                 >
-                  {skill.name}
-                </Tag>
-              ))}
-            </TagGroup.List>
-          </TagGroup>
-        </Tabs.Panel>
-
-        <Tabs.Panel className="pt-4 px-0" id="soft-skills">
-          <TagGroup
-            size="sm"
-            variant="surface"
-            aria-label="Tags"
-            selectionMode="single"
-          >
-            <TagGroup.List className={cn("overflow-auto h-fit")}>
-              {softSkills.map((skill: TagItem) => (
-                <Tag
-                  key={skill.id}
-                  id={skill.id}
-                  textValue={skill.name}
-                  className="font-light! px-2 py-0 text-nowrap"
+                  Hard
+                  <Tabs.Indicator />
+                </Tabs.Tab>
+                <Tabs.Tab
+                  id="soft-skills"
+                  className="h-fit text-secondary/85 font-normal"
                 >
-                  {skill.name}
-                </Tag>
-              ))}
-            </TagGroup.List>
-          </TagGroup>
-        </Tabs.Panel>
+                  Soft
+                  <Tabs.Indicator />
+                </Tabs.Tab>
+              </Tabs.List>
+            </Tabs.ListContainer>
+          </Card.Description>
+        </Card.Header>
+        <Card.Content className="p-0">
+          <Tabs.Panel id="hard-skills" className="px-0">
+            <TagGroup
+              variant="surface"
+              aria-label="Tags"
+              selectionMode="single"
+              className={cn("overflow-auto max-h-48", "", "")}
+            >
+              {" "}
+              <ScrollShadow visibility="auto" size={20}>
+                <TagGroup.List>
+                  {hardSkills.map((skill: TagItem) => (
+                    <Tag
+                      key={skill.id}
+                      id={skill.id}
+                      textValue={skill.name}
+                      className="font-light! px-2 py-0"
+                    >
+                      {skill.name}
+                    </Tag>
+                  ))}
+                </TagGroup.List>{" "}
+              </ScrollShadow>
+            </TagGroup>
+          </Tabs.Panel>
+
+          <Tabs.Panel className="pt-4 px-0" id="soft-skills">
+            <TagGroup
+              size="sm"
+              variant="surface"
+              aria-label="Tags"
+              selectionMode="single"
+            >
+              <ScrollShadow visibility="auto" size={20}>
+                <TagGroup.List className={cn("overflow-auto h-fit")}>
+                  {softSkills.map((skill: TagItem) => (
+                    <Tag
+                      key={skill.id}
+                      id={skill.id}
+                      textValue={skill.name}
+                      className="font-light! px-2 py-0 text-nowrap"
+                    >
+                      {skill.name}
+                    </Tag>
+                  ))}
+                </TagGroup.List>
+              </ScrollShadow>
+            </TagGroup>
+          </Tabs.Panel>
+        </Card.Content>
       </Tabs>
     </Card>
   );
--- src/app/components/content.components/ListCard.tools.tsx
+++ src/app/components/content.components/ListCard.tools.tsx
@@ -1,14 +1,16 @@
 import { ComponentBaseProps, TagItem } from "@/lib/types";
 import { cn } from "@/lib/utils";
-import { Card, Tag, TagGroup } from "@heroui/react";
+import { Card, ScrollShadow, Tag, TagGroup } from "@heroui/react";
 // import hardTools from "@data/tools.hard.json";
 import tools from "@data/tools.json";
 import { Tabs } from "@heroui/react";
+// TODO: https://v3.heroui.com/docs/react/components/chip ??
+// TODO: https://v3.heroui.com/docs/react/components/tag ??
 
 export default function ToolCard({ className }: ComponentBaseProps) {
   return (
     <Card id="tools" className={cn("glass", "glass-white", className, "", "")}>
-      <h2 className="text-secondary/85 self-center">Tools</h2>
+      <Card.Header className="text-secondary/85 self-center">Tools</Card.Header>
 
       <TagGroup
         variant="surface"
@@ -16,18 +18,23 @@ export default function ToolCard({ className }: ComponentBaseProps) {
         selectionMode="single"
         className={cn("overflow-x-hidden")}
       >
-        <TagGroup.List>
-          {tools.map((tool: TagItem) => (
-            <Tag
-              key={tool.id}
-              id={tool.id}
-              textValue={tool.name}
-              className="font-light! px-2 py-0"
-            >
-              {tool.name}
-            </Tag>
-          ))}
-        </TagGroup.List>
+        <ScrollShadow visibility="auto" size={20}>
+          {/* <ScrollShadow hideScrollBar className="pb-4" size={20}> */}
+          <Card.Content className="p-0">
+            <TagGroup.List>
+              {tools.map((tool: TagItem) => (
+                <Tag
+                  key={tool.id}
+                  id={tool.id}
+                  textValue={tool.name}
+                  className="font-light! px-2 py-0"
+                >
+                  {tool.name}
+                </Tag>
+              ))}
+            </TagGroup.List>
+          </Card.Content>
+        </ScrollShadow>
       </TagGroup>
     </Card>
   );
