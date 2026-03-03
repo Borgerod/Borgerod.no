ffc85021199fbc2f171030f89355ebc9e8bbe839
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2026-03-03 13:03:11 +0100

    feat: enhance WorkHistoryCard with job details display and link to descriptions

--- src/app/components/content.components/WorkHistoryCard.tsx
+++ src/app/components/content.components/WorkHistoryCard.tsx
@@ -1,8 +1,13 @@
 import { ComponentBaseProps } from "@/lib/types";
 import { cn } from "@/lib/utils";
-import { Card, Chip, Separator } from "@heroui/react";
+import { Card, Chip, Separator, Tag, TagGroup } from "@heroui/react";
 import workHistory from "@data/work_history.json";
+import Link from "next/link";
+// import { Link } from "@heroui/react";
 
+// import { ChevronsUpWide } from "@gravity-ui/icons";
+// import { ChevronsRight } from "@gravity-ui/icons";
+import { ArrowUpRightFromSquare, Link as LinkIcon } from "@gravity-ui/icons";
 type jobItem = {
   id: string;
   period: { start: string; end: string };
@@ -49,23 +54,167 @@ export default function WorkHistoryCard({ className }: ComponentBaseProps) {
 
         <Card.Description></Card.Description>
       </Card.Header>
-      <Card.Content className="text-xs font-light">
+      <Card.Content
+        className={cn(
+          "text-xs font-light",
+          "grid grid-cols-3",
+          // "grid grid-cols-[auto_1fr_auto]",
+          // "gap-1",
+          "grid grid-cols-[auto_auto_auto]",
+          // "grid grid-cols-[auto_2fr_1fr]",
+          // "auto-cols-auto",
+          // "grid-flow-col",
+          // "grid-flow-col-dense",
+
+          // "justify-items-between",
+          "justify-between",
+          "content-between",
+          // "gap-0",
+          // "xl:gap-2",
+
+          // "justify-evenly",
+          // "items-top",
+          "",
+          "",
+        )}
+      >
         {workHistory.map((job: jobItem) => (
-          <div key={job.id} id={job.id}>
-            <div>
-              {job.period.start} - {job.period.end}
+          // <Card.Content key={job.id} id={job.id} className="text-xs font-light ">
+          <>
+            <div
+              id="date-col"
+              // className="text-xs text-[8px] self-start font-thin text-nowrap"
+              // className="text-xs text-[9px] self-start font-thin text-nowrap"
+              className="text-xs text-[9px] self-start font-thin text-wrap xl:text-nowrap"
+              // className="text-xs text-[10px] self-start font-thin"
+            >
+              <span className="text-nowrap">{job.period.start}</span>
+              <span className="text-nowrap"> -</span>
+              <span className="text-nowrap">{job.period.end}</span>
             </div>
 
-            <div>
-              <p>{job.title}</p>
-              <p>{job.employer}</p>
-              <Chip className={cn(job.isProject ? "block" : "hidden")}>
-                Project
-              </Chip>
+            <Card
+              id="job-info-col"
+              variant="transparent"
+              className="text-nowrap overflow-hidden contents gap-0  p-0 rounded-none"
+              // className="text-nowrap overflow-visible contents gap-0 "
+            >
+              <Card.Header>
+                {/* <Card.Title className="text-accent-foreground/90 font-normal text-[12px] leading-none"> */}
+                <Card.Title className="text-accent-foreground/90 font-normal text-[13px] leading-none">
+                  {job.title}
+                </Card.Title>
+                {/* <Card.Description className="text-accent-foreground/80 text-xs  text-nowrap flex items-end text-[10px] leading-4 "> */}
+                {/* <Card.Description className="text-accent-foreground/80 text-xs text-nowrap flex items-start text-[10px] leading-none "> */}
+                {/* <Card.Description className="text-accent-foreground/80 text-xs text-nowrap flex items-start text-[10px]  "> */}
+                <Card.Description className="text-accent-foreground/80 text-xs text-nowrap flex items-start text-[11px]  ">
+                  {job.employer}
+                  <span className={cn(job.isProject ? "block" : "hidden")}>
+                    ,{" "}
+                    <Chip
+                      size="sm"
+                      variant="soft"
+                      className={cn(
+                        "font-light  w-fit",
+                        "bg-glass-gray/50", //todo custom color!
+                        "select-none",
+                        "font-thin",
+                        "text-accent-foreground/80",
+                        // "px-2 py-0 h-4",
+                        "px-1 py-0 h-4",
+                        "px-1 py-0 h-5 leading-none ",
+                        "text-[11px]",
+                        // "text-[10px]",
+                        "",
+                      )}
+                    >
+                      <Chip.Label>Project</Chip.Label>
+                    </Chip>
+                  </span>
+                </Card.Description>
+              </Card.Header>
+            </Card>
+
+            <div id="description-url-col read-more">
+              <Link href={job.description}>
+                <Chip
+                  size="sm"
+                  variant="primary"
+                  color="accent"
+                  key={job.id}
+                  id={`${job.id} read more`}
+                  className={cn(
+                    // "font-light px-2 py-0 h-4 w-fit",
+                    "font-light  h-4 w-fit",
+                    "font-light",
+                    " h-2",
+                    "text-[10px]",
+                    "w-fit",
+                    "p-0",
+                    "bg-glass-gray/50", //todo custom color!
+                    "text-accent-foreground/70",
+                    // "text-secondary",
+                    // "text-light-gray",
+                    // "text-glass-gray-undertone",
+                    // "text-gray-undertone/60",
+                    "select-none",
+                    "text-nowrap",
+                    "font-thin!",
+                    "2xl:px-2 ",
+                    "2xl:py-0 ",
+                    "2xl:pr-1",
+                    "2xl:py-2.5",
+                    // "lg:p-1",
+                    "",
+                    "",
+                  )}
+                >
+                  <Chip.Label className="flex items-center gap-1 p-0">
+                    <span className="hidden 2xl:block">Read more</span>
+                    {/* <LinkIcon className="size-3 " /> */}
+                    <LinkIcon className="size-2.5 " />
+                  </Chip.Label>
+                </Chip>
+              </Link>
             </div>
 
-            <div>{job.description}</div>
-          </div>
+            {/* <div id="description-url-col read-more">
+              <Link href={job.description}>
+                <Chip
+                  size="sm"
+                  variant="primary"
+                  color="accent"
+                  key={job.id}
+                  id={`${job.id} read more`}
+                  className={cn(
+                    // "font-light px-2 py-0 h-4 w-fit",
+                    "font-light  h-4 w-fit",
+                    "bg-glass-gray/50", //todo custom color!
+                    "text-accent-foreground/70",
+                    // "text-secondary",
+                    // "text-light-gray",
+                    // "text-glass-gray-undertone",
+                    // "text-gray-undertone/60",
+                    "select-none",
+                    "text-nowrap",
+                    "font-thin!",
+                    "2xl:px-2 ",
+                    "2xl:py-0 ",
+                    "2xl:pr-1",
+                    "2xl:py-2.5",
+                    // "lg:p-1",
+                    "",
+                    "",
+                  )}
+                >
+                  <Chip.Label className="flex items-center gap-1 p-0">
+                    <span className="hidden 2xl:block">Read more</span>
+                    <LinkIcon className="size-3" />
+                  </Chip.Label>
+                </Chip>
+              </Link>
+            </div> */}
+          </>
         ))}
       </Card.Content>
     </Card>
