d0c7a0afdf5b1f30c32bbe5d7bbd97ce6073ad05
Borgerod <borgerod@hotmail.com>
2026-03-06 11:08:44 +0100

    refactor: rename project.profile to project-profile feat: create ProjectProfile component reason: caused error in validator.ts when there was nothing to validate so i had to fill it with something
    note: profile-project might be removed later on.

---
+++ src/app/portfolio/project-profile/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@heroui/react";
+
+//> NOTHING TO SEE HERE - PROB WONT MAKE THIS
+export default function ProjectProfile() {
+  return (
+    <main>
+      <Card>
+        <Card.Header>Project-profile</Card.Header>
+      </Card>
+    </main>
+  );
+}
