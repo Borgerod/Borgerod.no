2739b853e80af288a77ec7ce94b11d14b37e466d
Borgerod <borgerod@hotmail.com>
2026-03-06 09:46:20 +0100

    feat: update image configuration to use remotePatterns for better handling of external images
    reason: "domains" are deprecated

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "heroui-assets.nyc3.cdn.digitaloceanspaces.com",
-      "c7a433b527dc2ae3c95c08a175c4984f.cdn.bubble.io",
-      "reboot.no",
-      "upload.wikimedia.org",
-      "mediavest.no",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "heroui-assets.nyc3.cdn.digitaloceanspaces.com",
+      },
+      {
+        protocol: "https",
+        hostname: "c7a433b527dc2ae3c95c08a175c4984f.cdn.bubble.io",
+      },
+      {
+        protocol: "https",
+        hostname: "reboot.no",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "mediavest.no",
+      },
     ],
   },
 };
