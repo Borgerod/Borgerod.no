8b8f4a840025fc5b3c828a4581657475c8efc9b7
Borgerod <borgerod@hotmail.com>
2026-03-06 11:48:53 +0100

    chore: remove unused demo and test pages from the project

--- src/app/blog/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-//TEST - The sole purpose of this is debugging
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
-  const { slug } = await params;
-  return <div>My Post: {slug}</div>;
-}
